ba33c19bf4398f5648d44245f9dd3e364205c90f
unknown <locadm@WSR3D-4.ttit.local>
2020-02-20 09:00:52 +0700

    release v1

---
+++ application/views/template_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Главная</title>
+</head>
+<body>
+<p><a href="/">Главная</a></p>
+<p><a href="/services">Услуги</a></p>
+<p><a href="/portfolio">Портфолио</a></p>
+<p><a href="/contacts">Контакты</a></p>
+<?php include 'application/views/'.$content_view; ?>
+</body>
+</html>
